9ab153540e527c1272a17c9994133cc7ee58935b
Your Name <you@example.com>
2023-05-17 00:12:00 +0300

    + IMGS GRID W FLEX

--- app/api.tsx
+++ app/api.tsx
@@ -4,8 +4,9 @@ export const API = {
 	    const apiUrl = ENV.API_HOST + "/fetch-images";
 	    const params = new URLSearchParams({
 	    	url,
+	    	t: + new Date, 
 	    	"min-width": minWidth,
-	    	"min-height": minHeight
+	    	"min-height": minHeight,
 	    });
 	    return await fetch(`${apiUrl}?${params}`, {
 	      method: "GET",
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,16 +2,17 @@ import type { V2_MetaFunction } from "@remix-run/node";
 import { Form, Button } from 'semantic-ui-react';
 import { useState } from "react";
 import { useSubmit } from "@remix-run/react";
-import { API } from '../api.tsx'
+import { API } from '../api.tsx';
+import { useEffect } from 'react';
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 }; 
 
 class FetchFormClass {
-    minWidth: string = "";
-    minHeight: string = "";
-    url: string = "";
+    minWidth: string = "200";
+    minHeight: string = "200";
+    url: string = "https://symfony.com/blog/new-in-symfony-3-3-optional-class-for-named-services";
     images: Array<String>[] = [];
 }
 
@@ -19,6 +20,16 @@ export default function Index() {
 
   const [state, setState] = useState(new FetchFormClass);
   const submitForm = useSubmit();
+
+  useEffect(() => {
+    API.getImages(
+      "http://localhost:3000/",
+      state.minWidth,
+      state.minHeight
+    ).then(images => { 
+      onChange(event, { name: "images", value: images });
+    });
+  }, []);
 
   const onChange = (evt, { name, value }) => {
     //Form submission happens here
@@ -38,9 +49,32 @@ export default function Index() {
     onChange(event, { name: "images", value: images });
     console.log("NEW STATE", state);
     //Form submission happens here
+
+
+  };
+  const gridStyle = {
+    display: "flex",
+    flexFlow: "row wrap",
+  };
+
+  const containerStyle = {
+    display: "inline-block",
+    height: "200px",
+    minWidth: "190px",
+    flex: "1",
+  };
+
+  const imgStyle = {
+    objectFit: "cover",
+    maxHeight: "200px",
+    width: "100%",
+    height: "100%",
   };
   return (
     <div>
+      <div>
+      <img src="http://localhost:8000/colors-gallery-storage/06094133e100e74b421c3da6e35be07b696c838d3c3bb10a34e1634f33723d87" />
+      </div>
       <Form onSubmit={onSubmit}>
         <Form.Input
           onChange={onChange}
@@ -62,11 +96,16 @@ export default function Index() {
         />
         <Button type="submit" name="url">Fetch</Button>
       </Form>
-      {
-        state.images.map(function(imageUrl, i){
-          return <img src={imageUrl} />;
-        })
-      }
+      <div style={gridStyle}>
+        {
+          state.images.map(function(imageUrl, i){
+            return <div style={containerStyle} class="image-container">
+                    <img key="i" style={imgStyle} src={imageUrl} />
+                  </div>;
+          })
+        }
+        <div style={{flex: 2}}></div>
+      </div>
     </div>
   );
 }
